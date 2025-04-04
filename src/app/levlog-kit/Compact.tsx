08323e899aa8b3f2a3e1e0b4b8506c99c3bb214f
Nick Hong <me@nickhong.xyz>
2025-04-04 16:53:45 +0800

    Add on view fading in

--- src/app/levlog-kit/Compact.tsx
+++ src/app/levlog-kit/Compact.tsx
@@ -1,26 +1,45 @@
+"use client";
+
 import { cn } from "@/utils/cn";
 import { advercaseBold, hostGroteskRegular } from "../font";
 import { HighlightedSpan } from "@/components/Text";
 import Image from "next/image";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 function Compact() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
-    <div
+    <motion.div
+      ref={ref}
       className={cn(
         "flex flex-col items-center justify-center gap-4 w-full max-w-screen-lg md:mx-auto px-8",
         hostGroteskRegular.className
       )}
       id="compact"
+      initial={{ opacity: 0, y: 50 }}
+      animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+      transition={{ duration: 0.5 }}
     >
-      <h2
+      <motion.h2
         className={cn(
           advercaseBold.className,
           "text-3xl drop-shadow-[0_0_2px_var(--accent)]"
         )}
+        initial={{ opacity: 0, y: 20 }}
+        animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
       >
         A <span className="text-(--accent)">Compact</span> Vlogging Kit
-      </h2>
-      <div className={cn("flex flex-col items-center justify-center")}>
+      </motion.h2>
+      <motion.div
+        className={cn("flex flex-col items-center justify-center")}
+        initial={{ opacity: 0, y: 20 }}
+        animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
         <p className="text-lg">
           The LeVlog kit is designed compact so that it can{" "}
           <HighlightedSpan className="text-base">
@@ -32,9 +51,21 @@ function Compact() {
           </HighlightedSpan>
           .
         </p>
-      </div>
-      <div className="w-full flex flex-col md:flex-row gap-4 md:py-8">
-        <div className="relative rounded-lg overflow-hidden w-full md:w-1/2">
+      </motion.div>
+      <motion.div
+        className="w-full flex flex-col md:flex-row gap-6 md:py-8"
+        initial={{ opacity: 0, y: 20 }}
+        animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+      >
+        <motion.div
+          className="relative rounded-lg overflow-hidden w-full md:w-1/2"
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={
+            isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.95 }
+          }
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
           <Image
             src="/vlog/renders/flat.png"
             alt="Compact LeVlog Kit"
@@ -42,8 +73,13 @@ function Compact() {
             height={1000}
             className="w-full h-full object-cover"
           />
-        </div>
-        <div className="flex flex-col gap-4 md:w-1/2">
+        </motion.div>
+        <motion.div
+          className="flex flex-col gap-4 md:w-1/2"
+          initial={{ opacity: 0, x: 20 }}
+          animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 20 }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+        >
           <div className="flex flex-col gap-2">
             <h3 className={cn(advercaseBold.className, "text-2xl")}>
               7mm thin
@@ -54,9 +90,9 @@ function Compact() {
               there.
             </p>
           </div>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
 
--- src/app/levlog-kit/Versatile.tsx
+++ src/app/levlog-kit/Versatile.tsx
@@ -1,18 +1,32 @@
-import { cn } from "@/utils/cn";
-import { stickConfigurations } from "./data";
-import { advercaseBold, hostGroteskRegular } from "../font";
+"use client";
+
 import { HighlightedSpan } from "@/components/Text";
+import { cn } from "@/utils/cn";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+import { advercaseBold, hostGroteskRegular } from "../font";
+import { stickConfigurations } from "./data";
 
 const StickConfig = ({
   config,
+  index,
+  isInView,
 }: {
   config: (typeof stickConfigurations)[number];
+  index: number;
+  isInView: boolean;
 }) => {
   return (
-    <li key={config.name} className="">
+    <motion.li
+      key={config.name}
+      className=""
+      initial={{ opacity: 0, y: 100 }}
+      animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+      transition={{ duration: 0.5, delay: index * 0.2 }}
+    >
       <strong
         className={cn(
-          "sm:hidden block text-center sm:pt-4 py-4",
+          "sm:hidden block text-2xl md:text-lg md:text-center sm:pt-4 py-4",
           advercaseBold.className
         )}
       >
@@ -40,17 +54,24 @@ const StickConfig = ({
           <p>{config.description}</p>
         </div>
       </div>
-    </li>
+    </motion.li>
   );
 };
 const Versatile = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
-    <div
+    <motion.div
       className={cn(
         "w-full max-w-screen-lg mx-auto z-20",
         hostGroteskRegular.className
       )}
+      initial={{ opacity: 0, x: 100 }}
+      animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }}
+      transition={{ duration: 0.5, delay: 0.5 }}
       id="versatile"
+      ref={ref}
     >
       <div className="flex flex-col items-center justify-center px-4 md:pb-16 mb-16 md:mt-0">
         <div className="flex flex-col md:items-center md:justify-center py-8 gap-4 px-4 md:px-8 xl:px-0">
@@ -66,13 +87,18 @@ const Versatile = () => {
             <HighlightedSpan>6 different</HighlightedSpan> use cases.
           </div>
           <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8 md:gap-4 w-full">
-            {stickConfigurations.map((config) => (
-              <StickConfig key={config.name} config={config} />
+            {stickConfigurations.map((config, index) => (
+              <StickConfig
+                key={config.name}
+                config={config}
+                index={index}
+                isInView={isInView}
+              />
             ))}
           </ul>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
